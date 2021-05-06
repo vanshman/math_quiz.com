2405d444ab4578b7b58ba5d0ca38f992a384a0f0
Vansh Mohan <69002944+vanshman@users.noreply.github.com>
2021-05-06 17:19:07 +0530

    nice
    nice

--- math1.js
+++ math1.js
@@ -1,7 +1,7 @@
 var player1, player2;
 function login(){
-    player1 = document.getElementById("player1_name");
-    player2 = document.getElementById("player2_name");
+    player1 = document.getElementById("player1_name").value;
+    player2 = document.getElementById("player2_name").value;
 
     localStorage.setItem("player1", player1);
     localStorage.setItem("player2", player2);
